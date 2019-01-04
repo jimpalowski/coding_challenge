4376ddb1019d8f4c3383d2562e45987c76531684
Jim Palowski <33942859+jimpalowski@users.noreply.github.com>
2019-01-03 23:49:11 -0800

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -32,6 +32,27 @@ for(let i = -300; i <= 0; i++){
 multiplesBy3()
 
 
+
+let arr = [3, 6, 9, 11, 28, 4]
+
+function findAverage(arr){
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  let average = sum/arr.length;
+  return average
+}
+
+findAverage(arr)
+
+
+
+
+
+
+
+
 //print "good moring!" 98 times"
 function beCheerful(){
     for(let i = 0; i <= 98; i++){
